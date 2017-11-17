3d24a8fb9b46bcd196fc2ff96df2d043b8c0d024
Jacob Wahlgren <jacob.wahlgren@gmail.com>
2017-11-17 18:21:59 +0100

    Use -1 to denote "null" index
    Not separating index 0 from no index was a bad idea and caused some
    weird code.

--- treef.c
+++ treef.c
@@ -14,8 +14,8 @@ struct arena {
 
 struct node {
     const char *name;
-    size_t sibling_idx;
-    size_t child_idx;
+    int sibling_idx;
+    int child_idx;
 };
 
 void arena_prepare_add(struct arena *arena)
@@ -32,31 +32,36 @@ void arena_prepare_add(struct arena *arena)
     }
 }
 
-size_t node_add(struct arena *arena, size_t parent_idx, const char *name, size_t last_sibling_idx)
+int node_add(struct arena *arena, int parent_idx, int last_sibling_idx, const char *name)
 {
     arena_prepare_add(arena);
 
     struct node *new = &arena->nodes[arena->used];
     new->name = name;
-    new->sibling_idx = 0;
-    new->child_idx = 0;
+    new->sibling_idx = -1;
+    new->child_idx = -1;
 
-    if (last_sibling_idx)
-        arena->nodes[last_sibling_idx].sibling_idx = arena->used;
-    else if (arena->used)
-        arena->nodes[parent_idx].child_idx = arena->used;
+    if (last_sibling_idx >= 0)
+        arena->nodes[last_sibling_idx].sibling_idx = (int) arena->used;
+    else if (parent_idx >= 0)
+        arena->nodes[parent_idx].child_idx = (int) arena->used;
 
-    return arena->used++;;
+    return (int) arena->used++;
 }
 
-size_t node_add_or_find(struct arena *arena, size_t parent_idx, const char *name, size_t len)
+int node_add_or_find(struct arena *arena, int parent_idx, const char *name, size_t len)
 {
-    size_t last_sibling_idx = 0;
+    int last_sibling_idx = -1;
 
     if (arena->used) {
-        size_t sibling_idx = arena->nodes[parent_idx].child_idx;
+        int sibling_idx = -1;
 
-        while (sibling_idx) {
+        if (parent_idx >= 0)
+            sibling_idx = arena->nodes[parent_idx].child_idx;
+        else
+            sibling_idx = 0;
+
+        while (sibling_idx >= 0) {
             // If this node already exists, no need to add it again
             if (!strncmp(arena->nodes[sibling_idx].name, name, len))
                 return sibling_idx;
@@ -66,11 +71,11 @@ size_t node_add_or_find(struct arena *arena, size_t parent_idx, const char *name
         }
     }
 
-    return node_add(arena, parent_idx, strndup(name, len), last_sibling_idx);
+    return node_add(arena, parent_idx, last_sibling_idx, strndup(name, len));
 }
 
 // Returns height at which the node was added.
-size_t path_add(struct arena *arena, size_t parent_idx, const char *path)
+size_t path_add(struct arena *arena, int parent_idx, const char *path)
 {
     while (*path == '/') path++;
 
@@ -86,38 +91,39 @@ size_t path_add(struct arena *arena, size_t parent_idx, const char *path)
     } else {
         // "abc/def": end = path + 3 -> end - path = 3 -> path[end - path] = '/'
         size_t len = (size_t) (end - path);
-        size_t new_parent_idx = node_add_or_find(arena, parent_idx, path, len);
+        int new_parent_idx = node_add_or_find(arena, parent_idx, path, len);
 
         return 1 + path_add(arena, new_parent_idx, end + 1);
     }
 }
 
-void print_tree(struct arena *arena, size_t *root_path, size_t depth, size_t root_idx)
+void print_siblings(struct arena *arena, int *tree_path, size_t depth, int node_idx)
 {
-    size_t child_idx = arena->nodes[root_idx].child_idx;
+    int child_idx = arena->nodes[node_idx].child_idx;
+    int next_idx = arena->nodes[node_idx].sibling_idx;
 
-    if (child_idx)
-        root_path[depth] = root_idx;
+    for (size_t anscestor = 0; anscestor < depth; anscestor++) {
+        if (arena->nodes[tree_path[anscestor]].sibling_idx < 0)
+            printf("    ");
+        else
+            printf("│   ");
+    }
 
-    while (child_idx) {
-        size_t next_idx = arena->nodes[child_idx].sibling_idx;
+    if (next_idx < 0) {
+        printf("└── ");
+    } else {
+        printf("├── ");
+    }
 
-        for (size_t anscestor = 1; anscestor <= depth; anscestor++) {
-            if (arena->nodes[root_path[anscestor]].sibling_idx)
-                printf("│   ");
-            else
-                printf("    ");
-        }
+    printf("%s\n", arena->nodes[node_idx].name);
 
-        if (next_idx) {
-            printf("├── ");
-        } else {
-            printf("└── ");
-        }
+    if (child_idx >= 0) {
+        tree_path[depth] = node_idx;
+        print_siblings(arena, tree_path, depth + 1, child_idx);
+    }
 
-        printf("%s\n", arena->nodes[child_idx].name);
-        print_tree(arena, root_path, depth + 1, child_idx);
-        child_idx = next_idx;
+    if (next_idx >= 0) {
+        print_siblings(arena, tree_path, depth, next_idx);
     }
 }
 
@@ -129,28 +135,27 @@ int main()
     size_t tree_height = 1;
     struct arena arena;
 
-    memset(&arena, 0, sizeof(arena));
-    node_add_or_find(&arena, 0, NULL, 0);
+    memset(&arena, 0, sizeof(struct arena));
 
     while ((nread = getline(&line, &linelen, stdin)) > 0) {
         if (line[nread - 1] == '\n')
             line[nread - 1] = '\0';
 
-        size_t height = path_add(&arena, 0, line);
+        size_t height = path_add(&arena, -1, line);
 
         if (height > tree_height)
             tree_height = height;
     }
 
-    size_t *path = malloc(sizeof(size_t) * tree_height);
+    int *path = malloc(sizeof(int) * tree_height);
 
     // If all paths have a common root component, we draw that as the
     // tree's root. Not if we only have a single lonely path -- that would
     // just echo that path.
-    if (arena.used > 2 && !arena.nodes[1].sibling_idx) {
+    /*if (arena.used > 1 && arena.nodes[0].sibling_idx < 0) {
         puts(arena.nodes[1].name);
-        print_tree(&arena, path, 0, 1);
-    } else {
-        print_tree(&arena, path, 0, 0);
+        print_siblings(&arena, path, 0, 1);
+    } else*/ {
+        print_siblings(&arena, path, 0, 0);
     }
 }
